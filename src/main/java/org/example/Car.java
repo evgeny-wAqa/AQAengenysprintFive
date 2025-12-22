ef40d1c5dae00afaaa623d3f624ebcbd1d0fc031
evgeny-wAqa <amount_list.@mail.ru>
2025-12-22 23:26:40 +0300

    Создай класс Car с полями brand, model, year Добавь конструктор и метод printCarInfo(), который выводит всю информацию о машине

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Car {
+    String brand;
+    String model;
+    int year;
+
+    public Car(String brand,String model,int year){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+    public void printCarInfo(){
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+}
