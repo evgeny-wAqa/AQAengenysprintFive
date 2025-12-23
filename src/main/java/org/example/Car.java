4dc1e0693b8928c76ed407b6def7fc84a06279bf
evgeny-wAqa <amount_list.@mail.ru>
2025-12-23 20:33:59 +0300

    Создай 3 объекта класса Car из предыдущей задачи и выведи информацию о каждом

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -5,12 +5,14 @@ public class Car {
     String model;
     int year;
 
-    public Car(String brand,String model,int year){
+    public Car(String brand,String model,int year) {
         this.brand = brand;
         this.model = model;
         this.year = year;
     }
-    public void printCarInfo(){
+
+
+    public void printCarInfo() {
         System.out.println("Brand: " + brand);
         System.out.println("Model: " + model);
         System.out.println("Year: " + year);
