c9dd2c9443b160acecc6aac7039b6b6d4ef20957
evgeny-wAqa <amount_list.@mail.ru>
2026-02-08 01:10:56 +0300

    начало seleni

---
+++ src/test/java/pageObject/GitHub.java
@@ -0,0 +1,27 @@
+package pageObject;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class GitHub {
+
+    private static final SelenideElement userNameorEmailInput = $(By.id("login_field"));
+    private static final SelenideElement userPasswordInput = $(By.id("password"));
+    private static final SelenideElement signUpButton = $(By.xpath( "//input[@value='Sign in']"));
+    private static final SelenideElement happyPathAuth = $(By.xpath("//a[@href='/new']"));
+    private static final SelenideElement invalidCredError = $(By.xpath("//div[contains(text(),'Incorrect username or password.')]") );
+
+
+    public void setuserNameorEmail (String username){
+        userNameorEmailInput.shouldBe(visible).setValue(username);}
+    public void setuserPassword (String password){
+        userPasswordInput.shouldBe(visible).setValue(password);
+    }
+    public void pushsignUpButton(){
+        signUpButton.shouldBe(visible).click();
+    }
+}
+
